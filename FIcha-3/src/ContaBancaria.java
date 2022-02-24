de907ff623bfb38708decb4c3f6bb4fcf504beb2
David Mata <52453479+davidrobert99@users.noreply.github.com>
2022-02-24 23:46:11 +0000

    Update Ficha 3

--- FIcha-3/src/ContaBancaria.java
+++ FIcha-3/src/ContaBancaria.java
@@ -13,6 +13,13 @@ public class ContaBancaria {
         dataAbertura = new Date();
     }
 
+    public ContaBancaria(String titular, double saldo) {
+        this.titular = titular;
+        this.saldo = saldo;
+        dataAbertura = new Date();
+    }
+
+
     public String getTitular() {
         return titular;
     }
@@ -43,5 +50,14 @@ public class ContaBancaria {
         if (valor<=saldo){
             saldo = saldo - valor;
         }
+    }
+
+    @Override
+    public String toString() {
+        return "ContaBancaria{" +
+                "titular='" + titular + '\'' +
+                ", saldo=" + saldo +
+                ", dataAbertura=" + dataAbertura +
+                '}';
     }
 }
